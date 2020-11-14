0f745ba662457f27fa1a1890021a69c373bd2241
Vishal Sengar <vishalsengardtu@gmail.com>
2020-11-14 15:32:36 +0530

    Custom Modules, Import Export

---
+++ index.js
@@ -0,0 +1,5 @@
+const Person = require('./person');
+
+const Person1 = new Person('Vishal', 21);
+
+Person1.greeting();
---
+++ person.js
@@ -0,0 +1,16 @@
+{ /* Module Wrapper Function
+  (function (exports, require, module, __filename, __dirname) { })
+  console.log(__filename, __dirname);*/ }
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`My name is ${this.name} and I am ${this.age} years old.`)
+  }
+}
+
+module.exports = Person;
